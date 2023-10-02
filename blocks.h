8b0339cb117ee880fac0bfd6e81569f1767ef0a8
Christo Rogers-Wright <vivi870123@gmail.com>
2023-10-02 21:55:42 +0100

    inital commit

--- blocks.h
+++ blocks.h
@@ -1,25 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰍛", "$HOME/.config/scripts/bar/mem.sh",	2,		0},
-
-	{"", "$HOME/.config/scripts/bar/swap.sh",	2,		0},
-
-    {"", "$HOME/.config/scripts/bar/cpu.sh",  2, 0},
-
-    {"", "$HOME/.config/scripts/bar/cputemp.sh",    2,      0},
-
-    {"", "$HOME/.config/scripts/bar/screentemp-gammastep.sh",  15,     0},
-
-    {"", "$HOME/.config/scripts/bar/network-traffic.sh",  5,      0},
-
-    {"", "$HOME/.config/scripts/bar/updates.sh", 60, 0},
-
-    {" ", "$HOME/.config/scripts/bar/uptime.sh", 60, 0},
-
-    {" ", "cat /tmp/keyboard_layout",                                  1,      0},
-
-	{"", "$HOME/.config/scripts/bar/date.sh",		10,		0},
+  /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"",	        "sb-torrent",	        20,	                7},
+  {"",	        "sb-volume",	        0,	                10},
+  {"",	        "sb-battery",	        5,	                3},
+  {"",	        "sb-internet",	        5,	                4},
+  {"",	        "sb-clock",	        60,	                1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
